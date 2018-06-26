fbd0bfae28a400902b2b6eee536439ac598f8381
marekbrainhub <marek@brainhub.pl>
2018-06-26 12:02:30 +0200

    04-keyboard

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <h1>Hangman</h1>
+  <div class="wrapper">
+    <h1>{{ store.word }}</h1>
+
+    <div class="keyboard">
+      <Key 
+        v-for="letter in alphabet"
+        :key="letter"
+        @click.native="checkLetter(letter)"
+        :guessed="guessed(letter)"
+      >{{ letter }}</Key>
+    </div>
 
     <div class="strikes" @click="store.strikes++">
       <Strike v-for="i in store.maxStrikes" :key="i" :order="i" />
@@ -10,19 +19,56 @@
 
 <script>
 import Strike from './Strike'
+import Key from './Key'
+
 import store from '@/store'
 
+function generateAlphabet() {
+  return [...Array(26)].map((_, i) => {
+    return String.fromCharCode(97 + i)
+  })
+}
+
 export default {
-  components: { Strike },
+  components: { Strike, Key },
+  methods: {
+    checkLetter(letter) {
+      this.store.guessedLetters.push(letter)
+    },
+    guessed(letter) {
+      return this.store.guessedLetters.includes(letter)
+    },
+  },
   data() {
     return {
       store,
+      alphabet: generateAlphabet()
     };
   },
 };
 </script>
 
-<style lang='stylus' scoped>
+<style lang='stylus'>
+*
+  margin 0
+  padding 0
+  box-sizing border-box
+
+h1
+  font-size 5rem
+
+.wrapper
+  display flex
+  flex-direction column
+  align-items center
+  justify-content center
+
+.keyboard
+  display flex
+  justify-content center
+  width 960px
+  flex-wrap wrap
+
 .strikes
   text-align center
   font-size 5rem
---
+++ src/components/Key.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="key" :class="{ guessed }">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['guessed'],
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<style lang='stylus'>
+.key
+  font-family 'Iosevka'
+  font-size 2rem
+  margin .5em
+  padding .5em
+  border 2px solid gray
+  border-radius 4px
+  user-select none
+  cursor pointer
+
+.guessed
+  background lightgray
+  cursor default
+</style>
