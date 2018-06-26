0944db2eeab1a770360e51bf5f4005068ca9af31
marekbrainhub <marek@brainhub.pl>
2018-06-26 09:47:39 +0200

    01-strikes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,37 @@
 <template>
-  <h1>Hangman</h1>
+  <div>
+    <h1>Hangman</h1>
+
+    <div class="strikes">
+      <span
+        v-for="i in maxStrikes"
+        :key="i"
+        :id="i"
+        :class="{ red: i <= strikes }"
+      >
+        ✖
+      </span>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-
+      strikes: 2,
+      maxStrikes: 5,
     };
   },
 };
 </script>
 
-<style lang='stylus'>
+<style lang='stylus' scoped>
+.strikes
+  text-align center
+  font-size 5rem
+  user-select none
 
+.red
+  color darken(red, 30%)
 </style>
---
+++ src/components/Strike.vue
@@ -0,0 +1,23 @@
+<template>
+  <span
+    :class="{ red: order <= strikes }"
+  >
+    ✖
+  </span>
+</template>
+
+<script>
+export default {
+  props: ['order'],
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<style lang='stylus' scoped>
+.red
+  color darken(red, 30%)
+</style>
